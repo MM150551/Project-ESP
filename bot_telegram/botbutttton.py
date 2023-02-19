f2b1672d574dd71eb6b08ead3063eb5cf0e3164b
Sherif-Ashraf421 <119701148+Sherif-Ashraf421@users.noreply.github.com>
2023-02-19 02:22:32 +0200

    bot version 2

--- bot_telegram/botbutttton.py
+++ bot_telegram/botbutttton.py
@@ -1,16 +1,28 @@
 from info import *
 list_btn = types.InlineKeyboardMarkup()
 list_btn2 = types.InlineKeyboardMarkup()
+list_btn3 = types.InlineKeyboardMarkup()
 b1= types.InlineKeyboardButton(text="yes,iam astra",callback_data="y")
 b2= types.InlineKeyboardButton(text="no",callback_data="n")
-b3= types.InlineKeyboardButton(text="/start",callback_data="start")
 b4= types.InlineKeyboardButton(text="time",callback_data="time")
 b5= types.InlineKeyboardButton(text="astra",callback_data="astra")
+b6= types.InlineKeyboardButton(text="EGYPT",callback_data="eg")
+b7= types.InlineKeyboardButton(text="UTC",callback_data="utc")
 list_btn.add(b1,b2)
-list_btn2.add(b3,b4,b5)
+list_btn2.add(b4,b5)
+list_btn3.add(b6,b7)
 def callback_result (call):
     if call.data == "y":
         Astra.send_message(call.message.chat.id, "good for you bro")
-    
     elif call.data == "n":
         Astra.send_message(call.message.chat.id, "bad for you")
+    elif call.data == "time":
+        Astra.send_message(call.message.chat.id,  "what time want ?", reply_markup=list_btn3)    
+    elif call.data == "astra":
+        Astra.send_message(call.message.chat.id, "you astra ?", reply_markup=list_btn)    
+    elif call.data == "eg":
+        Astra.send_message(call.message.chat.id, time_egy)
+    elif call.data == "utc":
+        Astra.send_message(call.message.chat.id, time_utc)        
+        
+         #Astra.reply_to(m,"you astra ?", reply_markup=list_btn)
--- bot_telegram/botcommands.py
+++ bot_telegram/botcommands.py
@@ -1,12 +1,16 @@
 from info import *
 from botbutttton import *
-def comnd_ban_fun(m):
+
+def command_fun(m):
     if m.text.lower() == "/ban" :
-        #Astra.send_message(m.chat.id, "البس بان " + "@"+ m.reply_to_message.from_user.username )
         bnn= Astra.ban_chat_member (m.chat.id, m.reply_to_message.from_user.id)
         if bnn  :
-            Astra.send_message(m.chat.id, " البس بان يازعيم" + "@" + str(m.reply_to_message.from_user.username ))
-def comnd_star_fun(m):
-    if m.text.lower() == "/start" :
-        #Astra.reply_to(m,"welcom: this is Astra bot project ")#, #reply_markup=list_btn)
+            Astra.send_message(m.chat.id, " GET OUT " + "@" + str(m.reply_to_message.from_user.first_name ))
+    elif m.text.lower() == "/start" :
         Astra.send_message(m.chat.id,"welcom " + m.from_user.first_name +" : this is Astra bot project \nto use bot you should write one of this words: ",reply_markup=list_btn2)
+
+    elif m.text.lower() == "/my_info" :
+        Astra.reply_to(m,m)  
+              
+
+        
